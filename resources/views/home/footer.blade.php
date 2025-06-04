4d018e9b6716a83c532d810d1de7864668edc79b
blueswayne133 <blueswayne133@gmail.com>
2025-06-04 14:03:56 -0700

    new changes

--- resources/views/home/footer.blade.php
+++ resources/views/home/footer.blade.php
@@ -1,7 +1,18 @@
 <!-- 
     - #FOOTER
   -->
-
+<!-- Smartsupp Live Chat script -->
+<script type="text/javascript">
+    var _smartsupp = _smartsupp || {};
+_smartsupp.key = '6ee2569bf89a8a2ea6907b841794fb259f68883d';
+window.smartsupp||(function(d) {
+  var s,c,o=smartsupp=function(){ o._.push(arguments)};o._=[];
+  s=d.getElementsByTagName('script')[0];c=d.createElement('script');
+  c.type='text/javascript';c.charset='utf-8';c.async=true;
+  c.src='https://www.smartsuppchat.com/loader.js?';s.parentNode.insertBefore(c,s);
+})(document);
+</script>
+<noscript> Powered by <a href=“https://www.smartsupp.com” target=“_blank”>Smartsupp</a></noscript>
 <footer class="footer">
     <div class="container">
 
